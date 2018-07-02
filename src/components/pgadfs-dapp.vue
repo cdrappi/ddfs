2eedb5612b4409a465cb3588e6a074406e53d691
Christian Drappi <christiandrappi@gmail.com>
2018-07-02 14:04:44 -0700

    pga dfs contract component

---
+++ src/components/pgadfs-dapp.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+ <hello-metamask/>
+ <pga-dfs/>
+</div>
+</template>
+<script>
+import HelloMetamask from '@/components/hello-metamask'
+import PgaDfs from '@/components/pga-dfs'
+export default {
+  name: 'pgadfs-dapp',
+  beforeCreate () {
+    console.log('registerWeb3 Action dispatched from pgadfs-dapp.vue')
+    this.$store.dispatch('registerWeb3')
+  },
+  components: {
+    'hello-metamask': HelloMetamask,
+    'pga-dfs': PgaDfs
+  }
+}
+</script>
+<style scoped>
+</style>
