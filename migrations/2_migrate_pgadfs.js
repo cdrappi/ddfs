1aa62062ac6463b8667bf1aec290f01a34028a08
Christian Drappi <christiandrappi@gmail.com>
2018-07-08 22:29:02 -0700

    .

--- migrations/2_migrate_pgadfs.js
+++ migrations/2_migrate_pgadfs.js
@@ -1,5 +1,8 @@
 var PgaDfs = artifacts.require("./PgaDfs.sol");
 
 module.exports = function(deployer) {
-    deployer.deploy(PgaDfs);
+    deployer.deploy(
+      PgaDfs,
+      { gas:6721975, value: 500000000000000000 }
+    );
 };
