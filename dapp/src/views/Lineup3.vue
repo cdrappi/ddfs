8d533d83b60fd0e2b7b1f8d4503a106e48cb5b6e
Christian Drappi <christiandrappi@gmail.com>
2018-07-14 13:22:26 -0700

    lineiup3

---
+++ dapp/src/views/Lineup3.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <multiselect
+      :options="options"
+      v-model="selected"
+      :multiple="true"
+      :searchable="true"
+      :close-on-select="false"
+      placeholder="Search"
+    >
+    </multiselect>
+  </div>
+</template>
+
+<script>
+  import Multiselect from 'vue-multiselect'
+  import mixin from '../libs/mixinViews'
+  import getGolfers from '../libs/getGolfers'
+  export default {
+    mixins: [mixin],
+    components: { Multiselect },
+    data () {
+      return {
+        selected: null,
+        options: getGolfers()
+      }
+    }
+  }
+</script>
