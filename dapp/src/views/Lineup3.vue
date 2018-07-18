ea626534372569065752022f7ce2a81caa3594c4
Christian Drappi <christiandrappi@gmail.com>
2018-07-17 17:38:30 -0700

    holy shit multiselect kinda works

--- dapp/src/views/Lineup3.vue
+++ dapp/src/views/Lineup3.vue
@@ -1,30 +1,59 @@
 <template>
   <div>
-
-    <ul class="resources-list">
-      <template v-for="(resource, index) in selectedGolfers">
-        <li class="resource-item" :data-index="index">
-          <div class="resource-info">
-            <div class="resource-title" :id="index">
-              <span>{{ resource.name }} ({{resource.pga_id}}) ... Ξ{{resource.eth_salary}} </span>
-            </div>
-          </div>
-          <div class="delete-controls" ><!-- v-on:click.prevent="removeDependency(index)"> -->
-            <i class="fa fa-times fa-fw"></i>
-          </div>
-        </li>
-      </template>
-      <button class="btn btn-primary" </button><!-- :disabled="disableSubmit" @click="performSubmit">Save to blockchain</button> -->
-        <strong v-show="submitting">Submitting...</strong>
-        <strong v-show="errorSubmit" class="text-danger">Error occurred!</strong>
+      <div>
+          TOTAL SALARY: {{ this.salarySum() }}  (max is 100)
+      </div>
+      <ul class="resources-list">
+          <template v-for="(resource, index) in selectedResources">
+              <li class="resource-item" :data-index="index">
+                  <div class="resource-info">
+                      <div class="resource-title" :id="index">
+                          <span>{{ resource.name }}</span>
+                          <span class="version">{{resource.eth_salary}}</span>
+                      </div>
+                      <div class="resource-description">
+                          <span>{{ resource.pga_id }}</span>
+                      </div>
+                  </div>
+                  <div class="delete-controls" v-on:click.prevent="removeDependency(index)">
+                      remove
+                      <i class="fa fa-times fa-fw"></i>
+                  </div>
+              </li>
+          </template>
     </ul>
+
+    <br>
+    <div>
+        PLAYER IDS:
+        <br>{{ this.getPlayerIdsForLineupHash() }}
+    </div>
+    <br>
+    <div>
+        LINEUP HASH:
+        <br>{{ this.getLineupHash() }}
+    </div>
+    <br>
+    <br>
+    <button class="btn btn-primary" :disabled="disableSubmit" @click="performSubmit">Save to blockchain</button>
+    <strong v-show="submitting">Submitting...</strong>
+    <strong v-show="errorSubmit" class="text-danger">Error occurred!</strong>
+
+    <br>
+    <br>
+    <br>
+
     <multiselect
       :options="options"
-      v-model="selectedGolfers"
+      :value="optionsProxy"
+      @input="updateSelected"
       :multiple="true"
       :searchable="true"
       :close-on-select="false"
-      placeholder="Search"
+      :clear-on-select="true"
+      :hide-selected="true"
+      placeholder="SEARCH"
+      :customLabel="formatResource"
     >
     </multiselect>
   </div>
@@ -34,14 +63,104 @@
   import Multiselect from 'vue-multiselect'
   import mixin from '../libs/mixinViews'
   import getGolfers from '../libs/getGolfers'
+  import Web3 from 'web3';
+  var web3 = new Web3();
   export default {
     mixins: [mixin],
     components: { Multiselect },
     data () {
       return {
-        selectedGolfers: null,
-        options: getGolfers()
+        selectedResources: [],
+        options: getGolfers(),
+        optionsProxy: [],
+        submitting: false,
+        errorSubmit: false,
+        successMessage: false,
       }
+    },
+    computed: {
+        /**
+         * It disables the submit button when the the name or userStatus are not filled
+         * or the submit button is pressed or the connection with the blockchin is
+         * not established.
+         */
+        disableSubmit() {
+            return (
+                this.salarySum() > 100
+                || this.selectedResources.length < 3
+                || this.selectedResources.length > 8
+                || this.submitting
+                || !this.blockchainIsConnected()
+            )
+        },
+
+    },
+    methods: {
+        updateSelected(value) {
+            value.forEach((resource) => {
+          	     // Adds selected resources to array
+                this.selectedResources.push(resource)
+            })
+            this.optionsProxy = []
+        },
+        salarySum() {
+            var totalSalary = 0;
+            var index;
+            for (index in this.selectedResources) {
+                totalSalary += this.selectedResources[index].eth_salary
+            }
+            return totalSalary
+        },
+        getPlayerIdsForLineupHash() {
+            var playerIdsToHash = '';
+            var index;
+            for (index in this.selectedResources) {
+                playerIdsToHash += String(this.selectedResources[index].pga_id);
+                if (!(index == this.selectedResources.length - 1)) {
+                    playerIdsToHash += ':';
+                }
+            }
+            return playerIdsToHash
+        },
+        getLineupHash() {
+            var playerIdsForLineupHash = this.getPlayerIdsForLineupHash();
+            return web3.sha3(playerIdsForLineupHash)
+        },
+        removeDependency(index) {
+            this.selectedResources.splice(index, 1)
+        },
+        formatResource(resource) {
+            return resource.eth_salary + '   -   ' + resource.pga_id + ': ' + resource.name
+        },
+        performSubmit() {
+            this.submitting = true
+            this.errorSubmit = false
+            this.successMessage = false
+            // calling the function registerUser of the smart contract
+            console.log('calling saveLineupHash L98')
+            // window.bc.contract().saveLineupHash(
+            //     this.userName,
+            //     this.userStatus,
+            //     {
+            //         from: window.bc.web3().eth.coinbase,
+            //         gas: 800000
+            //     },
+            //     (err, txHash) => {
+            //         if (err) {
+            //             console.error(err)
+            //             this.errorSubmit = true
+            //         }
+            //         else {
+            //             this.successMessage = true
+            //             // it emits a global event in order to update the top menu bar
+            //             Event.$emit('userregistered', txHash);
+            //             // the transaction was submitted and the user will be redirected to the
+            //             // profile page once the block will be mined
+            //             this.redirectWhenBlockMined()
+            //         }
+            //     }
+            // )
+        },
     }
   }
 </script>
@@ -50,7 +169,7 @@
 
 <style scoped>
 * {
-  font-family: 'Lato', 'Avenir', sans-serif;
+  font-family: monospace;
 }
 
 .resources-list {
